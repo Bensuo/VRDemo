1fb5d84c0d2c2bc920315fb942c4185b4dfe7c77
Stuart Adams <stuartdadams@gmail.com>
2017-12-01 11:18:34 +0000

    Update ShaderFactory.h

--- src/VRDemo/ShaderFactory.h
+++ src/VRDemo/ShaderFactory.h
@@ -7,6 +7,13 @@
 #include "Shader.h"
 #include <unordered_map>
 
+/*
+* Names: Stuart Adams and Ben Tracy
+* Student IDs: B00265262 & B00307589
+*
+* Acknowledgements: Code is from Stuart Adams' individual coursework for Advanced Graphics
+*/
+
 namespace Engine
 {
     namespace Content
