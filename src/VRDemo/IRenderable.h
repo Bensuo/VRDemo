c51f2909ec379fc48968adb4c5980b6e23444204
Stuart Adams <stuartdadams@gmail.com>
2017-12-01 11:11:02 +0000

    Update IRenderable.h

--- src/VRDemo/IRenderable.h
+++ src/VRDemo/IRenderable.h
@@ -2,6 +2,13 @@
 #ifndef IRENDERABLE_H
 #define IRENDERABLE_H
 
+/*
+* Names: Stuart Adams and Ben Tracy
+* Student IDs: B00265262 & B00307589
+*
+* Acknowledgements: Code is from Stuart Adams' individual coursework for Advanced Graphics
+*/
+
 namespace Engine
 {
     namespace Rendering
